082555906e9b5889808a34f794a1f6b8d8f82a4a
artem1kaminb <manvel.art231@gmail.com>
2026-04-09 14:37:57 +0300

    refactor: extract logic to app.js and create minimal main.js entry point

--- js/logger.js
+++ js/logger.js
@@ -1,26 +1,27 @@
-const isDev = import.meta.env.DEV; //визначає чи в нас режим розробки 'npm run dev' чи 'npm run build' 
+import pino from 'pino';
 
-export const logger = { //логування подій програми
+const isDev = import.meta.env.DEV;
 
+// Ініціалізація Pino 
+const log = pino({
+    browser: {
+        asObject: true //  об'єкти в консолі можна розгортати мишкою
+    }
+});
+
+export const logger = {
     /** @param {string} message - Опис події @param {any} [data=''] - Додаткові дані */
-
     info(message, data = '') { 
-        if (isDev) {
-            console.log(`[INFO] ${new Date().toISOString()}: ${message}`, data); //ISO формат часу для точності часових міток «рік-місяць-день»
-        }
+        log.info({ contextData: data }, message);
     },
 
-    /** @param {string} message - Контекст помилки@param {Error|string} error - Об'єкт помилки/виключення*/
-
+    /** @param {string} message - Контекст помилки @param {Error|string} error - Об'єкт помилки/виключення*/
     error(message, error) { 
-        console.error(`[ERROR] ${new Date().toISOString()}: ${message}`, error); //логування помилок
+        log.error({ err: error }, message);
     },
 
     /** Виводить попередження @param {string} message - Текст попередження.*/
-
     warn(message) { 
-        if (isDev) {
-            console.warn(`[WARN] ${message}`);
-        }
+        log.warn(message);
     }
 };
